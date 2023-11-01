bd9b7623e06c5ea5ed8310bfdb337ae81599871d
EduardoVP-0 <144479949+EduardoVP-0@users.noreply.github.com>
2023-11-01 14:53:12 -0600

    Agregar archivos de proyecto.

---
+++ 3JLSC-EduardoVelazquez-Examen1PracticoP2/3JLSC-EduardoVelazquez-Examen1PracticoP2.cpp
@@ -0,0 +1,50 @@
+// 3JLSC-EduardoVelazquez-Examen1PracticoP2
+//pila
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void llenarPila(vector<string>& pila, string elementos[], int capacidad, int& cima) {
+    if (cima < capacidad-1) {
+        cima++;
+        pila[cima] = elementos[cima];
+        llenarPila(pila, elementos, capacidad, cima);
+    }
+}
+
+int main()
+{
+    int cima = -1, i;
+    const int capacidad = 15;
+    vector<string>pila(capacidad);
+
+    //LA PILA SOLO RECIBIRA DATOS DE TIPO STRING 
+    //DONDE USTED PREVIAMENTE DEFINIRA LOS NOMBRES DE CADA ELEMENTO DE LA PILA
+    string elementos[] = { "Nombre01","Nombre02","Nombre03","Nombre04","Nombre05","Nombre06","Nombre07",
+        "Nombre08","Nombre09","Nombre10","Nombre11","Nombre12","Nombre13","Nombre14","Nombre15" };
+
+    //RECURSIVIDAD
+    llenarPila(pila, elementos, capacidad, cima);
+
+    //RECORRIDO DE LA PILA LIFO
+    //ITERATIVIDAD
+    cout << "Elementos de la pila: \n";
+    for (i = cima; i >= 0; i--) {
+        cout << pila[i] << endl;
+    }
+
+    //ELIMINAR LOS ELEMENTOS DE LA PILA
+    cout << "\nEliminando elementos de la pila... \n";
+    while (cima >= 0) {
+        cout << "Tamano de la pila antes de eliminar: " << pila.size() << endl;
+        cout << "Elemento " << pila[cima] << " ubicado en la posicion " << cima << " de la pila" << endl;
+        pila.pop_back();
+        cima--;
+        cout << "Elemento borrado. Tamano despues de eliminar: " << pila.size() << endl;
+    }
+
+    system("PAUSE");
+    return 0;
+}
